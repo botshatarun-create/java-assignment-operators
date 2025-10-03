d0cc4e57020e658a26d89ec22c34dd72b8d460cf
botshatarun-create <botshatarun@gmail.com>
2025-10-03 18:26:36 +0530

    Create q1.java
    q1 in java

---
+++ q1.java
@@ -0,0 +1,15 @@
+class DecimalToBinary {
+    public static void main(String[] args) {
+        int decimal = 56;
+        String binary = "";
+        int num = decimal;
+
+      for (;num > 0;) {
+            int remainder = num % 2;
+            binary = remainder + binary;
+            num = num / 2;
+        }
+
+        System.out.println("Binary of " + decimal + " = " + binary);
+    }
+}
