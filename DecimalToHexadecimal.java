845eeb99400bead46466edb4cc48522a291e090c
botshatarun-create <botshatarun@gmail.com>
2025-10-03 19:00:05 +0530

    Update DecimalToHexadecimal.java

--- DecimalToHexadecimal.java
+++ DecimalToHexadecimal.java
@@ -1,17 +1,14 @@
-public class DecimalToHexadecimal {
+ class DecimalToHex {
     public static void main(String[] args) {
-        int decimal = 254;
+        int decimal = 356;
         String hex = "";
-        char[] hexDigits = {'0','1','2','3','4','5','6','7','8','9',
-                            'A','B','C','D','E','F'};
 
-        int num = decimal;
-        while (num > 0) {
-            int remainder = num % 16;
-            hex = hexDigits[remainder] + hex;
-            num = num / 16;
+        while (decimal > 0) {
+            int r = decimal % 16;
+            hex = (r < 10 ? r : (char)('A' + r - 10)) + hex;
+            decimal = decimal / 16;
         }
 
-        System.out.println("Hexadecimal of " + decimal + " = " + hex);
+        System.out.println(hex);
     }
 }
